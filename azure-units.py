bfe939e5c993fe0f273381a10ee0313004991a4d
ModzNihaal <nihaal@pingsafe>
2023-02-07 15:19:48 +0530

    error column added in csv

--- azure-units.py
+++ azure-units.py
@@ -61,27 +61,61 @@ class PingSafeAzureUnitAudit:
 
     def add_result(self, k, v):
         with open(self.file_path, 'a') as f:
-            f.write(f'{k}, {v}\n')
+            error = json.dumps(v[1])
+            f.write(f'{k}, {v[0]}, {error}\n')
 
     def count_all(self):
         self.add_result("App Services", self.count_app_services())
-        accounts = self.list_storage_accounts()
-        count_storage_blob_container = 0
-        count_storage_file_share = 0
-        count_storage_queue = 0
-        count_storage_table = 0
-        for account in accounts:
-            key = self.get_storage_account_keys(account)
-            count_storage_blob_container += self.count_storage_blob_container(account, key)
-            count_storage_file_share += self.count_storage_file_share(account, key)
-            count_storage_queue += self.count_storage_queue(account, key)
-            count_storage_table += self.count_storage_table(account, key)
+        accounts, error = self.list_storage_accounts()
+        if accounts is None:
+            self.add_result("Storage Account", (0,error))
+            self.add_result("Storage Blob Container", (0,error))
+            self.add_result("Storage File Share", (0,error))
+            self.add_result("Storage Queue", (0,error))
+            self.add_result("Storage Table", (0,error))
+        else:
+            count_storage_blob_container = 0
+            count_storage_file_share = 0
+            count_storage_queue = 0
+            count_storage_table = 0
 
-        self.add_result("Storage Account", self.count_storage_accounts(accounts))
-        self.add_result("Storage Blob Container", count_storage_blob_container)
-        self.add_result("Storage File Share", count_storage_file_share)
-        self.add_result("Storage Queue", count_storage_queue)
-        self.add_result("Storage Table", count_storage_table)
+            errors_blob_container = ""
+            errors_file_share = ""
+            errors_storage_queue = ""
+            errors_storage_table = ""
+            for account in accounts:
+                key, error = self.get_storage_account_keys(account)
+                if key is None:
+                    errors = errors + error
+                else:
+                    results, error = self.count_storage_blob_container(account, key)
+                    if results is None:
+                        errors_blob_container = errors_blob_container + error
+                    else:
+                        count_storage_blob_container += results
+
+                    results, error = self.count_storage_file_share(account, key)
+                    if results is None:
+                        errors_file_share = errors_file_share + error
+                    else:
+                        count_storage_file_share += results
+
+                    results, error = self.count_storage_queue(account, key)
+                    if results is None:
+                        errors_storage_queue = errors_storage_queue + error
+                    else:
+                        count_storage_queue += results
+
+                    results, error = self.count_storage_table(account, key)
+                    if results is None:
+                        errors_storage_table = errors_storage_table + error
+                    else:
+                        count_storage_table += results
+            self.add_result("Storage Account", (self.count_storage_accounts(accounts), ""))
+            self.add_result("Storage Blob Container", (count_storage_blob_container,errors_blob_container))
+            self.add_result("Storage File Share", (count_storage_file_share,errors_file_share))
+            self.add_result("Storage Queue", (count_storage_queue,errors_storage_queue))
+            self.add_result("Storage Table", (count_storage_table,errors_storage_table))
 
         self.add_result("Virtual Machine", self.count_vm_instances())
         self.add_result("Compute Disks", self.count_disks())
@@ -102,7 +136,7 @@ class PingSafeAzureUnitAudit:
         self.add_result("Container Apps", self.count_containerapp())
         self.add_result("Kubernetes Cluster", self.count_kubernetes_clusters())
         self.add_result("CosmosDB Account", self.count_cosmosdb())
-        self.add_result("Total Resource", self.total_resource_count)
+        self.add_result("Total Resource", (self.total_resource_count,""))
         print("results stored at", self.file_path)
 
     def count_storage_blob_container(self, account, key):
@@ -110,64 +144,63 @@ class PingSafeAzureUnitAudit:
         success, output = call_with_output(f"az storage container list --account-key {key} --account-name {account} {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return None, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
 
     def count_storage_file_share(self, account, key):
         print(f"getting storage account ({account}) data for count_storage_file_share")
         success, output = call_with_output(f"az storage share list --account-key {key} --account-name {account} {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return None, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
 
     def count_storage_queue(self, account, key):
         print(f"getting storage account ({account}) data for count_storage_queue")
         success, output = call_with_output(f"az storage queue list --account-key {key} --account-name {account} {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return None, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
 
     def count_storage_table(self, account, key):
         print(f"getting storage account ({account}) data for count_storage_table")
         success, output = call_with_output(f"az storage table list --account-key {key} --account-name {account} {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return None, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
 
     def count_app_services(self):
         print('getting data for count_app_services')
         success, output = call_with_output(f"az appservice ase list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
 
     def list_storage_accounts(self):
         success, output = call_with_output(f"az storage account list {self.subscription_flag}  --query '[].name' --output json")
         if not success:
-            print("Failed with error: ",output)
-            return 0
-        return json.loads(output)
+            return None, output
+        return json.loads(output), ""
     
     def get_storage_account_keys(self, account):
         success, output = call_with_output(f"az storage account keys list --account-name {account} {self.subscription_flag}  --query '[0].value' --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
-        return json.loads(output)
+            return None, output
+        return json.loads(output), ""
     
     def count_storage_accounts(self, storage_accounts_list):
         print('getting data for count_storage_accounts')
@@ -179,30 +212,30 @@ class PingSafeAzureUnitAudit:
         success, output = call_with_output(f"az vm list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
     
     def count_vm_instance_scale_sets(self):
         print('getting data for count_vm_instance_scale_sets')
         success, output = call_with_output(f"az vmss list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
     
     def count_disks(self):
         print('getting data for count_disks')
         success, output = call_with_output(f"az disk list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
 
 
     def count_vpc_networks(self):
@@ -210,180 +243,180 @@ class PingSafeAzureUnitAudit:
         success, output = call_with_output(f"az network vnet list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
 
     def count_sql_server(self):
         print('getting data for count_sql_server')
         success, output = call_with_output(f"az sql server list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
         
     def count_postgres_server(self):
         print('getting data for count_postgres_server')
         success, output = call_with_output(f"az postgres server list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
     
     def count_redis(self):
         print('getting data for count_redis')
         success, output = call_with_output(f"az redis list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
 
     def count_mysql_server(self):
         print('getting data for count_mysql_server')
         success, output = call_with_output(f"az mysql server list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
     
     def count_network_watcher(self):
         print('getting data for count_network_watcher')
         success, output = call_with_output(f"az network watcher list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
     
     def count_network_security_group(self):
         print('getting data for count_network_security_group')
         success, output = call_with_output(f"az network nsg list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
     
     def count_key_vault(self):
         print('getting data for count_key_vault')
         success, output = call_with_output(f"az keyvault list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
 
     def count_kubernetes_clusters(self):
         print('getting data for count_kubernetes_clusters')
         success, output = call_with_output(f"az aks list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
 
     def count_cdn_profile(self):
         print('getting data for count_cdn_profile')
         success, output = call_with_output(f"az cdn profile list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
 
     def count_container_registry(self):
         print('getting data for count_container_registry')
         success, output = call_with_output(f"az acr list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
     
     def count_container_instance(self):
         print('getting data for count_container_instance')
         success, output = call_with_output(f"az container list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
     
     def count_containerapp(self):
         print('getting data for count_containerapp')
         success, output = call_with_output(f"az containerapp list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
     
     def count_cosmosdb(self):
         print('getting data for count_cosmosdb')
         success, output = call_with_output(f"az cosmosdb list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
     
     def count_network_watcher(self):
         print('getting data for count_network_watcher')
         success, output = call_with_output(f"az network watcher list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
     
     def count_container(self):
         print('getting data for count_container')
         success, output = call_with_output(f"az container list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
     
     def count_app_gateway(self):
         print('getting data for count_app_gateway')
         success, output = call_with_output(f"az network application-gateway list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
     
     def count_load_balancers(self):
         print('getting data for count_load_balancers')
         success, output = call_with_output(f"az network lb list {self.subscription_flag} --output json")
         if not success:
             print("Failed with error: ",output)
-            return 0
+            return 0, output
         j = json.loads(output)
         self.total_resource_count += len(j)
-        return len(j)
+        return len(j), ""
 
 if __name__ == '__main__':
     subscriptions = SUBSCRIPTIONS if len(SUBSCRIPTIONS) > 0 else [None]
