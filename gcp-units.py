47d0f45041758c847e56725446aea7e4f386ec87
Harsh Singhvi <harsh.singhvi@sentinelone.com>
2024-03-28 12:27:10 +0530

    GCP Sizing script

--- gcp-units.py
+++ gcp-units.py
@@ -93,48 +93,15 @@ class PingSafeGCPUnitAudit:
             f.write(f'{k}, {v}\n')
 
     def count_all(self):
-        self.add_result("Load Balancers", self.count_load_balancers())
-        self.add_result("Compute Instances", self.count_compute_instances())
-        self.add_result("Disks", self.count_disks())
-        self.add_result("VPC Networks", self.count_vpc_networks())
-        self.add_result("Firewalls", self.count_firewalls())
-        self.add_result("Managed DNS Zones", self.count_managed_dns_zones())
-
-        # All iam users also includes service accounts
-        all_iam_users = self.count_all_iam_users()
-        all_service_account_users = self.count_all_service_account_users()
-        self.total_resource_count -= all_service_account_users
-        self.add_result("IAM Users", all_iam_users - all_service_account_users)
-        self.add_result("Service Account Users", all_service_account_users)
-
-        self.add_result("Kubernetes Clusters", self.count_kubernetes_clusters())
-        self.add_result("Alert Policies", self.count_alert_policies())
-        self.add_result("Log Sinks", self.count_log_sinks())
-        self.add_result("SQL Instances", self.count_sql_instances())
-        self.add_result("Storage Buckets", self.count_storage_buckets())
-
-        self.add_result("Pub Sub Topics", self.count_pub_sub_topics())
-        self.add_result("Spanner Instances", self.count_spanner_instances())
-        self.add_result("Cloud Functions", self.count_cloud_functions())
-
-        big_query_datasets, big_query_tables = self.count_big_query_datasets_tables()
-        self.add_result("Big Query Datasets", big_query_datasets)
-        self.add_result("Big Query Tables", big_query_tables)
+        self.add_result("GCP Compute Instance", self.count_compute_instances())
+        self.add_result("GCP Kubernetes Cluster (GKE)", self.count_kubernetes_clusters())
+        self.add_result("GCP Cloud Function", self.count_cloud_functions())
+        self.add_result("GCP Cloud Run", self.count_cloud_run())
+        self.add_result("GCP Artifact Repository (only docker repositories)", self.count_artifact_repository_docker())
+        self.add_result("GCP Container Repository", self.count_container_repository())
 
         self.add_result('TOTAL', self.total_resource_count)
         print("results stored at", self.file_path)
-
-    def count_load_balancers(self):
-        print('getting data for count_load_balancers')
-        if not self.is_api_enabled(["compute.googleapis.com"]):
-            return 0
-        output = subprocess.check_output(
-            "gcloud compute forwarding-rules list --format json",
-            text=True, shell=True
-        )
-        j = json.loads(output)
-        self.total_resource_count += len(j)
-        return len(j)
 
     def count_compute_instances(self):
         print('getting data for count_compute_instances')
@@ -142,78 +109,6 @@ class PingSafeGCPUnitAudit:
             return 0
         output = subprocess.check_output(
             "gcloud compute instances list --format json",
-            text=True, shell=True
-        )
-        j = json.loads(output)
-        self.total_resource_count += len(j)
-        return len(j)
-
-    def count_disks(self):
-        print('getting data for count_disks')
-        if not self.is_api_enabled(["compute.googleapis.com"]):
-            return 0
-        output = subprocess.check_output(
-            "gcloud compute disks list --format json",
-            text=True, shell=True
-        )
-        j = json.loads(output)
-        self.total_resource_count += len(j)
-        return len(j)
-
-    def count_vpc_networks(self):
-        print('getting data for count_vpc_networks')
-        if not self.is_api_enabled(["compute.googleapis.com"]):
-            return 0
-        output = subprocess.check_output(
-            "gcloud compute networks list --format json",
-            text=True, shell=True
-        )
-        j = json.loads(output)
-        self.total_resource_count += len(j)
-        return len(j)
-
-    def count_firewalls(self):
-        print('getting data for count_firewalls')
-        if not self.is_api_enabled(["compute.googleapis.com"]):
-            return 0
-        output = subprocess.check_output(
-            "gcloud compute firewall-rules list --format json",
-            text=True, shell=True
-        )
-        j = json.loads(output)
-        self.total_resource_count += len(j)
-        return len(j)
-
-    def count_managed_dns_zones(self):
-        print('getting data for count_managed_dns_zones')
-        if not self.is_api_enabled(["dns.googleapis.com"]):
-            return 0
-        output = subprocess.check_output(
-            "gcloud dns managed-zones list --format json",
-            text=True, shell=True
-        )
-        j = json.loads(output)
-        self.total_resource_count += len(j)
-        return len(j)
-
-    def count_all_iam_users(self):
-        print('getting data for count_all_iam_users')
-        if not self.is_api_enabled(["iam.googleapis.com", "orgpolicy.googleapis.com"]):
-            return 0
-        output = subprocess.check_output(
-            f"gcloud projects get-iam-policy {self.project_id} --flatten=\"bindings[].members\" --format json",
-            text=True, shell=True
-        )
-        j = json.loads(output)
-        self.total_resource_count += len(j)
-        return len(j)
-
-    def count_all_service_account_users(self):
-        print('getting data for count_all_service_account_users')
-        if not self.is_api_enabled(["iam.googleapis.com", "orgpolicy.googleapis.com"]):
-            return 0
-        output = subprocess.check_output(
-            "gcloud iam service-accounts list --format json",
             text=True, shell=True
         )
         j = json.loads(output)
@@ -232,78 +127,6 @@ class PingSafeGCPUnitAudit:
         self.total_resource_count += len(j)
         return len(j)
 
-    def count_alert_policies(self):
-        print('getting data for count_alert_policies')
-        if not self.is_api_enabled(["monitoring.googleapis.com"]):
-            return 0
-        output = subprocess.check_output(
-            "gcloud alpha monitoring policies list --format json",
-            text=True, shell=True
-        )
-        j = json.loads(output)
-        self.total_resource_count += len(j)
-        return len(j)
-
-    def count_log_sinks(self):
-        print('getting data for count_log_sinks')
-        if not self.is_api_enabled(["logging.googleapis.com"]):
-            return 0
-        output = subprocess.check_output(
-            "gcloud logging sinks list --format json",
-            text=True, shell=True
-        )
-        j = json.loads(output)
-        self.total_resource_count += len(j)
-        return len(j)
-
-    def count_sql_instances(self):
-        print('getting data for count_sql_instances')
-        if not self.is_api_enabled(["sqladmin.googleapis.com"]):
-            return 0
-        output = subprocess.check_output(
-            "gcloud sql instances list --format json",
-            text=True, shell=True
-        )
-        j = json.loads(output)
-        self.total_resource_count += len(j)
-        return len(j)
-
-    def count_storage_buckets(self):
-        print('getting data for count_storage_buckets')
-        if not self.is_api_enabled(["storage.googleapis.com"]):
-            return 0
-        output = subprocess.check_output(
-            f"gsutil ls -p {self.project_id}",
-            text=True, shell=True
-        )
-        j = list(filter(lambda x: len(x.strip()) > 0, output.split('\n')))
-        self.total_resource_count += len(j)
-        return len(j)
-
-    def count_pub_sub_topics(self):
-        print('getting data for count_pub_sub_topics')
-        if not self.is_api_enabled(["pubsub.googleapis.com"]):
-            return 0
-        output = subprocess.check_output(
-            "gcloud pubsub topics list --format json",
-            text=True, shell=True
-        )
-        j = json.loads(output)
-        self.total_resource_count += len(j)
-        return len(j)
-
-    def count_spanner_instances(self):
-        print('getting data for count_spanner_instances')
-        if not self.is_api_enabled(["spanner.googleapis.com"]):
-            return 0
-        output = subprocess.check_output(
-            "gcloud spanner instances list --format json",
-            text=True, shell=True
-        )
-        j = json.loads(output)
-        self.total_resource_count += len(j)
-        return len(j)
-
     def count_cloud_functions(self):
         print('getting data for count_cloud_functions')
         if not self.is_api_enabled(["cloudfunctions.googleapis.com"]):
@@ -315,32 +138,50 @@ class PingSafeGCPUnitAudit:
         j = json.loads(output)
         self.total_resource_count += len(j)
         return len(j)
-
-    def count_big_query_datasets_tables(self):
-        print('getting data for count_big_query_datasets_tables')
-        if not self.is_api_enabled(["bigquery.googleapis.com"]):
-            return 0, 0
-        dataset_count, table_count = 0, 0
+    def count_cloud_run(self):
+        print('getting data for count_cloud_run')
+        if not self.is_api_enabled(["run.googleapis.com"]):
+            return 0
         output = subprocess.check_output(
-            f"bq ls --project_id {self.project_id} --format json",
+            f"gcloud run services list --format json",
             text=True, shell=True
         )
-        if output is None or len(output.strip()) == 0:
-            output = "[]"
-        datasets = json.loads(output)
-        dataset_count = len(datasets)
-        for dataset in datasets:
-            output = subprocess.check_output(
-                f"bq ls --project_id {self.project_id} --max_results 10000 --format json {dataset['id']}",
-                text=True, shell=True
-            )
-            if output is None or len(output.strip()) == 0:
-                output = "[]"
-            tables = json.loads(output)
-            table_count += len(tables)
-        self.total_resource_count += dataset_count + table_count
-        return dataset_count, table_count
+        j = json.loads(output)
 
+        # TODO remove latter if needed
+        output2 = subprocess.check_output(
+            f"gcloud run jobs list --format json",
+            text=True, shell=True
+        )
+        i = json.loads(output2)
+        self.total_resource_count += len(i)
+
+        self.total_resource_count += len(j)
+        return len(j) + len(i)
+
+    def count_artifact_repository_docker(self):
+        print('getting data for count_artifact_repository_docker')
+        if not self.is_api_enabled(["artifactregistry.googleapis.com"]):
+            return 0
+        output = subprocess.check_output(
+            f"gcloud artifacts repositories list --filter=\"format=docker\" --format json",
+            text=True, shell=True
+        )
+        j = json.loads(output)
+        self.total_resource_count += len(j)
+        return len(j)
+
+    def count_container_repository(self):
+        print('getting data for count_container_repository')
+        if not self.is_api_enabled(["storage-api.googleapis.com"]):
+            return 0
+        output = subprocess.check_output(
+            f"gcloud container images list --format json",
+            text=True, shell=True
+        )
+        j = json.loads(output)
+        self.total_resource_count += len(j)
+        return len(j)
 
 GCP_CF_LOCATIONS = [
     'us-west1',
